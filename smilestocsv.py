8b0e654cee3d0614ed0ec0d70df53d94751f7028
Anubha <rk.universalpandey@gmail.com>
2023-03-21 14:05:42 +0100

    smiles to csv

--- smilestocsv.py
+++ smilestocsv.py
@@ -6,6 +6,10 @@ parser=argparse.ArgumentParser(description="convert smiles file to CSV file")
 
 
 # add the arguments to further process
+parser.add_argument('-i','input_file',help="Input file name for conversion")
+parser.add_argument('-o','output_file',help="Output file name for conversion")
+
+# parse the arguments 
 args=parser.args()
 
 
